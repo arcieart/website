d77631687e3cd61ead7da804e012145c31fdfcde
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2025-06-04 21:50:49 +0530

    feat: add firebase functions

---
+++ functions/src/firebase-admin.ts
@@ -0,0 +1,13 @@
+import * as admin from "firebase-admin";
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_ADMIN!);
+
+// Check if Firebase admin is already initialized to prevent multiple instances
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  });
+}
+
+export const firestore = admin.firestore;
---
+++ functions/src/index.ts
@@ -0,0 +1,4 @@
+// https://github.com/robisim74/firebase-functions-typescript-starter/blob/master/functions/src/index.ts
+
+import dotenv from "dotenv";
+dotenv.config();
