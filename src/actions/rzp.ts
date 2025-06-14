7c8a324dc70da3fa8c4c079ed4daf6d69a9dd390
Shamoil Arsiwala <myarcieart@gmail.com>
2025-06-14 23:53:01 +0530

    revert: test rzp comments

--- src/actions/rzp.ts
+++ src/actions/rzp.ts
@@ -4,10 +4,6 @@
 import Razorpay from "razorpay";
 
 export const createRazorpayOrder = async (amount: number, dbId: string) => {
-  console.log("Creating Razorpay order", amount, dbId);
-  console.log("RZP_KEY_SECRET", process.env.RZP_KEY_SECRET);
-  console.log("NEXT_PUBLIC_RZP_KEY_ID", process.env.NEXT_PUBLIC_RZP_KEY_ID);
-
   const rzp = new Razorpay({
     key_id: process.env.NEXT_PUBLIC_RZP_KEY_ID!,
     key_secret: process.env.RZP_KEY_SECRET!,
